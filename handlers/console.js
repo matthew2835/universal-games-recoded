730d40630378ff647619ffe73842fee7359ceba9
matthew2835 <59455288+matthew2835@users.noreply.github.com>
2020-01-07 09:15:48 -0500

    Add files via upload

---
+++ handlers/console.js
@@ -0,0 +1,7 @@
+module.exports = (bot) => {
+    let prompt = process.openStdin()
+    prompt.addListener("data", res => {
+        let x = res.toString().trim().split(/ +/g)
+            bot.channels.get("657301360936484884").send(x.join(" "));
+        });
+    }
---
+++ index.js
@@ -0,0 +1,9 @@
+const { Client, Collection } = require("discord.js");
+const { token } = require("./botconfig.json");
+const bot = new Client();
+
+bot.prefix = ".";
+["aliases", "commands"].forEach(x => bot[x] = new Collection());
+["console", "command", "event"].forEach(x => require(`./handlers/${x}`)(bot));
+
+bot.login(token);
